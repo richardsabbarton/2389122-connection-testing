7b020238ff2ef6dd9678f80535814e791612dfa9
Richard Sabbarton <richard.sabbarton@vonage.com>
2023-11-20 14:48:04 +0000

    First iteration

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const OpenTok = require('opentok')
 
+
+console.log(process.env['API'+'47807831'])
 
 console.log('Neru Instance')
 const neru = require('neru-alpha').neru
@@ -15,6 +18,56 @@ app.get('/', (req, res) => {
   res.send('Express and Node are working Fine')
 })
 
+app.get('/session/:apiKey/:room',async (req, res)=>{
+  try {
+    const apiKey = req.params['apiKey']
+    const roomName = req.params['room']
+    let apiSecret = process.env['API' + apiKey]
+
+    const opentok = new OpenTok(apiKey, apiSecret)
+    
+    console.log("Selected API Key: " + apiKey)
+    console.log("getting session ID from State Engine for Room: " + roomName)
+    
+    const  sessionId  = await state.hget(apiKey, roomName)
+    console.log(sessionId)
+
+    if (sessionId !== null) {
+      console.log(`Generating token for existing session: ${sessionId}`)
+      const data = {
+        sessionId: sessionId,
+        apiKey: apiKey,
+      }
+      data.token = opentok.generateToken(sessionId)
+      res.json(data)
+    } else {
+      const data = {
+        apiKey: apiKey,
+      }
+
+      const sessionOptions = {
+        mediaMode: "routed"
+      }
+
+      opentok.createSession(sessionOptions, (error, session)=>{
+        if(error){
+          console.log(error)
+        } else {
+          data.sessionId = session.sessionId
+          data.token = opentok.generateToken(data.sessionId)
+          res.json(data)
+          console.log("setting session Id in state engine")
+          let saveInfo = []
+          saveInfo[roomName] = data.sessionId
+          state.hset(apiKey, saveInfo)
+        }
+      })
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+})
 
 app.get('/_/health', async (req, res) => {
   res.sendStatus(200);
